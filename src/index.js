28c4e4cc9f00bc92a201aec1ff0f797ccebc9aa6
irwinet <ydamianelguera@gmail.com>
2021-06-04 21:03:47 -0500

    se implemento metodo de validacion de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+//pruebas unitarias
+var test = '4556364607935606';
+console.log(validator.isValid(test));
+console.log(validator.maskify(test));
+
+//manejo del input
+document.getElementById("cargar-tarjeta").addEventListener("click", () => {
+    document.getElementById("validar").style.display = "block";
+    document.getElementById("validar-nuevo").style.display = "none";
+    document.getElementById("presentacion").style.display = "none";
+    //Limpiar contenido
+    document.getElementById("frmValidarTarjeta").reset();
+});
+
+//manejo de boton Validar 
+document.getElementById("btn-validar").addEventListener("click", (e) => {
+    e.preventDefault(); //Evita que vaya a la accion del formulario
+    document.getElementById("presentacion").style.display = "none";
+    document.getElementById("validar").style.display = "none";
+    document.getElementById("validar-nuevo").style.display = "block";
+
+    //accion donde se captura el nombre
+    let name = document.getElementById("nombre").value;
+    document.getElementById("validar-nombre").innerHTML = name;
+
+    //validar tarjeta correcta o incorrecta
+    var creditCardNumber = document.getElementById("numero-tarjeta").value;
+    var isValid = validator.isValid(creditCardNumber);
+    var mensajeValid = "Invalido";
+    if (isValid) {
+        mensajeValid = "Valido";
+    }
+
+    document.getElementById("resultado-tarjeta").innerHTML = "La tarjeta ingresada es: " + mensajeValid;
+});
+
+//manejo de boton Validar nueva Tarjeta
+document.getElementById("nueva-tarjeta").addEventListener("click", () => {
+    document.getElementById("presentacion").style.display = "block";
+    document.getElementById("validar-nuevo").style.display = "none";
+    document.getElementById("validar").style.display = "none";
+    //Limpiar contenido
+    document.getElementById("frmValidarTarjeta").reset();
+});
+
+
+//funcion para validar que el input solo acepte numeros
+document.getElementById("numero-tarjeta").addEventListener("keypress", function(event) {
+    function solonumeros(evt) {
+        let charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode == 46 || charCode > 31 && (charCode < 48 || charCode > 57)) {
+            evt.preventDefault();
+            return false;
+        }
+        return true;
+
+    }
+    return solonumeros(event);
+}, false);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
+    // ...
+    isValid(creditCardNumber) {
+        const array = creditCardNumber.split(""); //cada digito de la tarjeta colocamos en un array
+        var sumaTotal = 0;
+        //aqui se llena el algoritmo
+        let arrayInvertido = array.reverse(); //invertimos el array
+
+        for (let i = 0; i < arrayInvertido.length; i++) {
+            if (i % 2 != 0) {
+                var opr = parseInt(arrayInvertido[i]) * 2;
+
+                if (opr >= 10) {
+                    opr = sumDigits(opr);
+                }
+
+                sumaTotal = sumaTotal + opr;
+            } else {
+                sumaTotal = sumaTotal + parseInt(arrayInvertido[i]);
+            }
+
+        }
+
+
+        var result = sumaTotal % 10;
+
+        if (result == 0)
+            return true;
+        else
+            return false;
+
+    },
+
+    maskify(creditCardNumber) {
+
+        //se mostrara los cuatro ultimos digitos de la tarjeta
+        return '############5616';
+    }
 };
+
+function sumDigits(n) {
+    let numArr = n.toString().split("");
+
+    let sum = numArr.reduce(function(a, b) {
+        return parseInt(a) + parseInt(b);
+    }, 0);
+
+    return sum;
+}
 
 export default validator;
